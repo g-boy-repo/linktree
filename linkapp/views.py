d7cfadaa66e00cedb9cbdcff5891939594d8a596
G-boy75 <glul71597@gmail.com>
2023-10-24 18:27:39 -0400

    Creating New Links with the Create View Class Based View

--- linkapp/views.py
+++ linkapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from .models import Profile, Link
 
@@ -8,3 +8,14 @@ from .models import Profile, Link
 
 class LinkListView(ListView):
     model = Link
+    # template called model_list.html -> link_list.html
+
+
+class LinkCreateView(CreateView):
+    # create forms.py file & form
+    # check if this was a post or get request
+    # return an empty form or  save the form data
+    model = Link
+    fields = "__all__"
+    success_url = reverse_lazy("link-list")
+    # template called model_form.html -> link_form.html
